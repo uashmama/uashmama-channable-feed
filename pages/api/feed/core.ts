516b858f898312c311446c9cadc9f1cf571f04bc
Pier <pier@intersezione.com>
2021-02-03 17:48:58 +0100

    core - fixed product prices procedure + defined xml procedure

--- pages/api/feed/core.ts
+++ pages/api/feed/core.ts
@@ -2,11 +2,12 @@ import axios from 'axios'
 import _ from 'lodash'
 import {
   Product,
-  GoogleProduct,
-  GooglePrice,
+  ChannableProduct,
+  ChannablePrice,
   FPReturnObj,
   GetRange,
   UpdateProduct,
+	GetProductsXML,
 } from './@types'
 import CLayer from '@commercelayer/js-sdk'
 import { FilterProducts, AddPrices } from './@types/index'
@@ -14,140 +15,172 @@ import { FilterProducts, AddPrices } from './@types/index'
 const getRange: GetRange = (amountFloat, currencySymbol) => {
   let range = ''
   if (amountFloat < 25) {
-    range = `< 25${currencySymbol}`
+    range = `< 25 ${currencySymbol}`
   }
   if (amountFloat >= 25 && amountFloat <= 50) {
-    range = `25-50${currencySymbol}`
+    range = `25-50 ${currencySymbol}`
   }
   if (amountFloat >= 50 && amountFloat <= 100) {
-    range = `50-100${currencySymbol}`
+    range = `50-100 ${currencySymbol}`
   }
   if (amountFloat > 100) {
-    range = `> 100${currencySymbol}`
+    range = `> 100 ${currencySymbol}`
   }
   return range
 }
 
-const updateProduct: UpdateProduct = ({ products, singleProduct }) => {
-  const index = _.findIndex(
-    products,
-    (pr) => pr.skuCode === singleProduct.skuCode
-  )
-  if (products[index]) {
-    products[index].price = {
-      currency: singleProduct.currencyCode,
-      value: `${singleProduct.amountFloat}`,
-    }
-    const currencySymbol = singleProduct.formattedAmount.charAt(0)
-    products[index].customLabel1 = getRange(
-      singleProduct.amountFloat,
-      currencySymbol
-    )
-    delete products[index].skuCode
-    return products[index]
-  }
-  return null
-}
-
 const filterProducts: FilterProducts = (response) => {
-  const { PERMALINK_BASE, PRODUCT_PREFIX, IMG_QUERY } = process.env
-  const ids: string[] = []
-  const returnObj: FPReturnObj = {
+	const { DT_LOCALE, PERMALINK_BASE, PRODUCT_PREFIX, IMG_QUERY } = process.env
+	const skus: string[] = []
+	const returnObj: FPReturnObj = {
     skuCodes: [],
     products: [],
   }
   response.data.allCategories.map((cat) => {
     cat.products.map((p: Product): any => {
-      const gPrice: GooglePrice = {
-        value: '',
-        currency: '',
+      const cPrice: ChannablePrice = {
+				value: '',
+				currency: '',
+				currencySymbol: ''
       }
-      const id = p.id
       const pName = `${p.name.charAt(0).toUpperCase()}${p.name
         .slice(1)
         .toLocaleLowerCase()}`
       const name = PRODUCT_PREFIX ? `${PRODUCT_PREFIX} ${pName}` : pName
       const material = p.material ? p.material.name : ''
-      const skuCode = _.first(p.variants)?.code
+      
       const offerId = p.permalink
       const permalink = PERMALINK_BASE
         ? `${PERMALINK_BASE}${p.permalink}`
-        : p.permalink
-      const getImg = _.first(p.images)?.url || ''
-      const additionalImageLinks = p.images?.slice(1).map((i) => {
-        if (i.url && IMG_QUERY) return `${i.url}?${IMG_QUERY}`
-        return i.url
-      })
-      const img = getImg && IMG_QUERY ? `${getImg}?${IMG_QUERY}` : ''
-      if (skuCode && ids.indexOf(p.id) === -1) {
-        returnObj.skuCodes.push(skuCode)
-      }
-      const gProduct: GoogleProduct = {
-        skuCode,
-        id,
-        title: name,
-        link: permalink,
-        imageLink: img,
-        price: gPrice,
-        description: p.description,
-        availability: 'in stock',
-        offerId,
-        contentLanguage: 'en',
-        targetCountry: 'GB',
-        channel: 'online',
-        customLabel0: 'blank',
-        customLabel1: '> 100',
-        material,
-        condition: 'new',
-        additionalImageLinks,
-      }
-      if (ids.indexOf(p.id) === -1) {
-        returnObj.products.push(gProduct)
-        ids.push(p.id)
-      }
+				: p.permalink
+
+			const productImg = _.first(p.images)?.url || ''
+			const productAdditionalImgs = p.images?.slice(1).map((i) => {
+				if (i.url && IMG_QUERY) return encodeURIComponent(`${i.url}?${IMG_QUERY}`)
+				return encodeURIComponent(i.url)
+			})
+
+			const mainSku = _.first(p.variants)?.code || ''
+
+			if (mainSku.length > 0 && returnObj.skuCodes.indexOf(mainSku) === -1) {
+				returnObj.skuCodes.push(mainSku)
+			}
+
+			for (let j in p.variants) {
+				let v = p.variants[j]
+				const skuCode = v.code
+
+				const variantImg = _.first(v.images)?.url || ''
+				const getImg = variantImg != '' ? variantImg : productImg
+				const imgLink = getImg && IMG_QUERY ? `${getImg}?${IMG_QUERY}` : ''
+
+				const variantAdditionalImgs = v.images?.slice(1).map((i) => {
+					if (i.url && IMG_QUERY) return encodeURIComponent(`${i.url}?${IMG_QUERY}`)
+					return encodeURIComponent(i.url)
+				})
+				
+				const gProduct: ChannableProduct = {
+					skuCode,
+					sku: v.code,
+					mainSku,
+					title: name,
+					link: permalink,
+					catlink: cat.permalink,
+					imageLink: encodeURIComponent(imgLink),
+					color: v.color && v.color.name ? v.color.name : '',
+					price: cPrice,
+					description: p.description,
+					availability: 'in stock',
+					offerId,
+					contentLanguage: DT_LOCALE,
+					customLabel0: 'blank',
+					customLabel1: '> 100',
+					material,
+					condition: 'new',
+					additionalImageLinks: variantAdditionalImgs.length > 0 ? variantAdditionalImgs : productAdditionalImgs,
+				}
+			
+				if (skus.indexOf(v.code) === -1) {
+					returnObj.products.push(gProduct)
+					skus.push(v.code)
+				}
+			}
     })
-  })
+	})
   return returnObj
 }
 
 const addPrices: AddPrices = ({ skuCodes, products }) => {
-  const getPrices = CLayer.Price.where({
+	const getPrices = CLayer.Price.where({
     skuCodeIn: skuCodes.join(','),
 	}).all()
   const updatedProductsWithPrices = getPrices.then(async (prices) => {
-		let productsWithPrice: GoogleProduct[] = []
+		let pricesData: any[] = []
+
 		prices.toArray().map((p) => {
-			const item = updateProduct({
-        products,
-        singleProduct: p,
-      })
-      if (item) {
-        productsWithPrice.push(item)
+			pricesData[p.skuCode] = {
+				value: p.amountFloat,
+				currency: p.currencyCode,
+				currencySymbol: p.formattedAmount.charAt(0)
 			}
-    })
-    const pageCount = prices.pageCount()
-    let nextPrs = prices
-    if (prices.hasNextPage() && pageCount) {
-      for (let count = 1; count < pageCount; count++) {
-        productsWithPrice = []
-        // @ts-ignore
-        nextPrs = await nextPrs.nextPage()
-        nextPrs.toArray().map((p) => {
-					const item = updateProduct({
-            products,
-            singleProduct: p,
-          })
-          if (item) {
-            productsWithPrice.push(item)
-          }
-        })
-      }
+		})
+		const pageCount = prices.pageCount()
+		let nextPrs = prices
+		if (prices.hasNextPage() && pageCount) {
+			for (let count = 1; count < pageCount; count++) {
+				nextPrs = await nextPrs.nextPage()
+				nextPrs.toArray().map((p) => {
+					pricesData[p.skuCode] = {
+						value: p.amountFloat,
+						currency: p.currencyCode,
+						currencySymbol: p.formattedAmount.charAt(0)
+					}
+				})
+			}
 		}
-		return productsWithPrice
-  }).catch(error => {
-		console.info('getPricesError', error)
-	})
+
+		for (let index in products) {
+			products[index].price = {
+				currency: pricesData[products[index].mainSku].currency,
+				value: pricesData[products[index].mainSku].value
+			};
+
+			products[index].customLabel1 = getRange(
+				pricesData[products[index].mainSku].value,
+				pricesData[products[index].mainSku].currencySymbol
+			);
+		}
+
+		return products
+	});
 	return updatedProductsWithPrices
+}
+
+const getProductsXML: GetProductsXML = ( products ) => {
+	let xmlOutput = `<?xml version='1.0' encoding='utf-8'?><items>`;
+	for(let i in products) {
+		let p = products[i]
+		xmlOutput += `<item>
+			<additional_image_link>${p.additionalImageLinks.join(',')}</additional_image_link>
+			<availability>${p.availability}</availability>
+			<brand>Uashmama</brand>
+			<color>${p.color}</color>
+			<condition>${p.condition}</condition> 
+			<description>${p.description}</description>
+			<id>${p.sku}</id>
+			<image_link>${p.imageLink}</image_link>
+			<link>${p.link}</link>
+			<material>${p.material}</material>
+			<price>${p.price.value}</price>
+			<product_type>${p.catlink}</product_type>
+			<sale_price>${p.price.value}</sale_price>
+			<size></size>
+			<stock></stock>
+			<title>${p.title}</title>
+		</item>`;
+	}
+	xmlOutput += `</items>`;
+	return xmlOutput;
 }
 
 const getData = async () => {
@@ -191,18 +224,19 @@ const getData = async () => {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${DT_AUTH}`,
     },
-		data: JSON.stringify({ query: `{ allCategories(first:100, locale: ${DT_LOCALE}, filter: { catalogo: {eq:false} visible: {eq: true} allProductsCategory: {eq: false} enabledMarkets: {anyIn: ${DT_MARKET}} }){ title products { id name permalink description variants { code } material { name } images { url } } } }` }),
+		data: JSON.stringify({
+			query: `{ allCategories(first:100, locale: ${DT_LOCALE}, filter: { catalogo: {eq:false} visible: {eq: true} allProductsCategory: {eq: false} enabledMarkets: {anyIn: ${DT_MARKET}} }){ title permalink products { id name permalink description variants { code images { url } color { name } } material { name } images { url } } } }` }),
 	}).then(({ data }) => {
 		let products = filterProducts(data)
 		const productsWithPrice = addPrices(products).then(productsWithPriceData => {
 			return productsWithPriceData
 		})
 		return productsWithPrice
-	}).catch(error => {
-		console.info('datoCMSError', error)
 	});
 
-	return datoCms;
+	// console.info('product data', datoCms[0])
+
+	return getProductsXML(datoCms);
 }
 
 export default getData
